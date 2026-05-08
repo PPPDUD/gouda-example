1b9effcda8e9f02b8fda2a54e6e329697423af50
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-05-08 19:39:29 -0400

    Update install.sh to use symlinks

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cp my-awesome-example /usr/local/bin/
-chmod +x /usr/local/bin/my-awesome-example
-echo Example installed to /usr/local/bin!
+ln -s my-awesome-example /usr/local/bin/my-awesome-example
+echo Example installed!
